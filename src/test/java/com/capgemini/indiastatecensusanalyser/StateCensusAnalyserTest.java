538399fcdcc013c3c49746b26b2462104beaf79c
Your Name <you@example.com>
2020-10-25 19:01:31 +0530

    [shashi] Added TC2.1: added to ensure no of entries of state codes

--- src/test/java/com/capgemini/indiastatecensusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/capgemini/indiastatecensusanalyser/StateCensusAnalyserTest.java
@@ -1,4 +1,22 @@
 package com.capgemini.indiastatecensusanalyser;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class StateCensusAnalyserTest {
+	private static final String CENSUS_DATA_PATH = "C:\\Users\\shashi7\\eclipse-workspace\\indiastatecensusanalyser\\src\\main\\java\\com\\capgemini\\resources\\IndianStateCodes.csv";
+	private StateCensusAnalyser stateCensusAnalyser;
+
+	@Before
+	public void init() {
+		stateCensusAnalyser = new StateCensusAnalyser();
+	}
+
+	@Test
+	public void givenCensusCSVFile_ReturnsCorrectNoOfEntries() {
+		int noOfEntries = stateCensusAnalyser.loadCensusData(CENSUS_DATA_PATH);
+		assertEquals(29, noOfEntries);
+	}
 }
